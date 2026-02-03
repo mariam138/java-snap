b16588f7ae0c0363d9a8d702c726d7d3f71ce6b5
Mariam <mariam@Mariams-MacBook-Pro.local>
2026-02-03 15:18:38 +0000

    set up vars and toString method

---
+++ src/main/java/Card.java
@@ -0,0 +1,14 @@
+public class Card {
+    String suit;
+    String symbol;
+    int value;
+
+    @Override
+    public String toString() {
+        return "Card{" +
+                "suit='" + suit + '\'' +
+                ", symbol='" + symbol + '\'' +
+                ", value=" + value +
+                '}';
+    }
+}
