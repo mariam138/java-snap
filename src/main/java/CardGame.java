fc70aec480732aae23707b1f186b78e1f82787ef
Mariam <mariam@Mariams-MacBook-Pro.local>
2026-02-03 16:10:05 +0000

    remove cards arraylist from constructor

--- src/main/java/CardGame.java
+++ src/main/java/CardGame.java
@@ -4,9 +4,12 @@ public class CardGame {
     ArrayList<Card> cards = new ArrayList<>(52);
     String nameOfGame;
 
-    //    We pass in the card deck and name of game to be played when creating a new card game instance
-    public CardGame(ArrayList<Card> cards, String name) {
-        this.cards = cards;
+    // We pass in the name of game to be played when creating a new card game instance
+    public CardGame(String name) {
         this.nameOfGame = name;
+    }
+
+    public ArrayList<Card> getCards() {
+        return cards;
     }
 }
