422ceab7d56bf992f958869e9700221a7ac34cbf
Mariam <mariam@Mariams-MacBook-Pro.local>
2026-02-03 15:42:18 +0000

    rewrite toString() method

--- src/main/java/Card.java
+++ src/main/java/Card.java
@@ -1,19 +1,17 @@
 import java.util.ArrayList;
+import java.util.Arrays;
 
 public class Card {
     String[] suit = {"\u2660", "\u2663", "\u2665", "\u2666"};
     String[] symbol = {"2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A"};
     int[] value = {2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14};
 
-//    ArrayList<suit> suits = new ArrayList<>();
-
-
     @Override
     public String toString() {
         return "Card{" +
-                "suit='" + suit + '\'' +
-                ", symbol='" + symbol + '\'' +
-                ", value=" + value +
+                "suit=" + Arrays.toString(suit) +
+                ", symbol=" + Arrays.toString(symbol) +
+                ", value=" + Arrays.toString(value) +
                 '}';
     }
 }
