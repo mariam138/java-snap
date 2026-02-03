57a155797975d30a9ca70be864774bde30d04d75
Mariam <mariam@Mariams-MacBook-Pro.local>
2026-02-03 15:54:49 +0000

    create constructor for CardGame

--- src/main/java/CardGame.java
+++ src/main/java/CardGame.java
@@ -2,4 +2,11 @@ import java.util.ArrayList;
 
 public class CardGame {
     ArrayList<Card> cards = new ArrayList<>(52);
+    String nameOfGame;
+
+    //    We pass in the card deck and name of game to be played when creating a new card game instance
+    public CardGame(ArrayList<Card> cards, String name) {
+        this.cards = cards;
+        this.nameOfGame = name;
+    }
 }
