5d53f163276e4f6c1d4b5e74c0e940018e7bc884
Mariam <mariam@Mariams-MacBook-Pro.local>
2026-02-03 15:40:35 +0000

    create string array of symbols

--- src/main/java/Card.java
+++ src/main/java/Card.java
@@ -2,7 +2,7 @@ import java.util.ArrayList;
 
 public class Card {
     String[] suit = {"\u2660", "\u2663", "\u2665", "\u2666"};
-    String symbol;
+    String[] symbol = {"2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A"};
     int value;
 
 //    ArrayList<suit> suits = new ArrayList<>();
