f53223bb773e21cf5c7e4a4d95907add24477e01
Mariam <mariam@Mariams-MacBook-Pro.local>
2026-02-03 15:41:26 +0000

    create int array of card values

--- src/main/java/Card.java
+++ src/main/java/Card.java
@@ -3,7 +3,7 @@ import java.util.ArrayList;
 public class Card {
     String[] suit = {"\u2660", "\u2663", "\u2665", "\u2666"};
     String[] symbol = {"2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A"};
-    int value;
+    int[] value = {2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14};
 
 //    ArrayList<suit> suits = new ArrayList<>();
 
