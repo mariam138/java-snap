a9eef2df5c970617815da591e1a1b5c1434de042
Mariam <mariam@Mariams-MacBook-Pro.local>
2026-02-04 14:50:51 +0000

    create constructor

--- src/main/java/Card.java
+++ src/main/java/Card.java
@@ -1,10 +1,13 @@
-import java.util.ArrayList;
-import java.util.Arrays;
-
 public class Card {
     String suit;
     String symbol;
     int value;
+
+    public Card(String suit, String symbol, int value) {
+        this.suit = suit;
+        this.symbol = symbol;
+        this.value = value;
+    }
 
     @Override
     public String toString() {
