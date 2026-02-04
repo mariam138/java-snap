bfce2cc761a42ce5f1e64e79bf146045615bced2
Mariam <mariam@Mariams-MacBook-Pro.local>
2026-02-04 14:40:56 +0000

    remove array values, update toString()

--- src/main/java/Card.java
+++ src/main/java/Card.java
@@ -2,16 +2,16 @@ import java.util.ArrayList;
 import java.util.Arrays;
 
 public class Card {
-    String[] suit = {"\u2660", "\u2663", "\u2665", "\u2666"};
-    String[] symbol = {"2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A"};
-    int[] value = {2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14};
+    String suit;
+    String symbol;
+    int value;
 
     @Override
     public String toString() {
         return "Card{" +
-                "suit=" + Arrays.toString(suit) +
-                ", symbol=" + Arrays.toString(symbol) +
-                ", value=" + Arrays.toString(value) +
+                "suit='" + suit + '\'' +
+                ", symbol='" + symbol + '\'' +
+                ", value=" + value +
                 '}';
     }
 }
