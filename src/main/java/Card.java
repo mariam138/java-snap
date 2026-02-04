b6fb34d10740345c49a828865007d5e71192e604
Mariam <mariam@Mariams-MacBook-Pro.local>
2026-02-04 20:07:54 +0000

    create cardValue prop and update toString()

--- src/main/java/Card.java
+++ src/main/java/Card.java
@@ -1,20 +1,31 @@
 public class Card {
-    String suit;
-    String symbol;
-    int value;
+   private String suit;
+//    String symbol;
+//    int value;
+    private CardGame.CardValue cardValue;
 
-    public Card(String suit, String symbol, int value) {
+    public Card(String suit, CardGame.CardValue cardValue) {
         this.suit = suit;
-        this.symbol = symbol;
-        this.value = value;
+        this.cardValue = cardValue;
+//        this.symbol = symbol;
+//        this.value = value;
+
     }
+
+//    @Override
+//    public String toString() {
+//        return "Card{" +
+//                "suit='" + suit + '\'' +
+//                ", symbol='" + symbol + '\'' +
+//                ", value=" + value +
+//                '}';
+//    }
 
     @Override
     public String toString() {
         return "Card{" +
                 "suit='" + suit + '\'' +
-                ", symbol='" + symbol + '\'' +
-                ", value=" + value +
+                ", cardValue=" + cardValue +
                 '}';
     }
 }
