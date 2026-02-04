1d057449dafe9bb89cc96baf5c4415c903415a57
Mariam <mariam@Mariams-MacBook-Pro.local>
2026-02-04 14:38:35 +0000

    make arraylist private

--- src/main/java/CardGame.java
+++ src/main/java/CardGame.java
@@ -1,7 +1,7 @@
 import java.util.ArrayList;
 
 public class CardGame {
-    ArrayList<Card> cards = new ArrayList<>(52);
+    private ArrayList<Card> cards = new ArrayList<>(52);
     String nameOfGame;
 
     // We pass in the name of game to be played when creating a new card game instance
@@ -13,3 +13,19 @@ public class CardGame {
         return cards;
     }
 }
+
+// public enum CardValue
+//{
+//  TWO(2),
+//  THREE(3),
+//  FOUR(4),
+//  FIVE(5),
+//  SIX(6),
+//  SEVEN(7),
+//  EIGHT(8),
+//  NINE(9),
+//  TEN(10),
+//  JACK(11),
+//  QUEEN(12),
+//  KING(13),
+//  ACE(14); }
