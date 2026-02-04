164d7524d1cb317c1e58ed8193b6e4c87ccc1aca
Mariam <mariam@Mariams-MacBook-Pro.local>
2026-02-04 16:52:58 +0000

    create enum for card values

--- src/main/java/CardGame.java
+++ src/main/java/CardGame.java
@@ -13,6 +13,31 @@ public class CardGame {
     private static final String[] SYMBOLS = {"2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A"};
     private static final int[] VALUES = {2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14};
 
+    public enum CardValue {
+        TWO(2),
+        THREE(3),
+        FOUR(4),
+        FIVE(5),
+        SIX(6),
+        SEVEN(7),
+        EIGHT(8),
+        NINE(9),
+        TEN(10),
+        J(11),
+        Q(12),
+        K(13),
+        A(14);
+
+        private int cardValue;
+        private CardValue(int value) {
+            this.cardValue = value;
+        }
+
+        public int getCardValue() {
+            return cardValue;
+        }
+    }
+
     // We pass in the name of game to be played when creating a new card game instance
     public CardGame(ArrayList<Card> cards, String name) {
         this.nameOfGame = name;
