59f5f26c7674e4cd77aff5f3bb5031d4b5adfd2b
Mariam <mariam@Mariams-MacBook-Pro.local>
2026-02-05 15:56:04 +0000

    override dealCard method

--- src/main/java/Snap.java
+++ src/main/java/Snap.java
@@ -2,4 +2,21 @@ public class Snap extends CardGame{
     public Snap(String name) {
         super(name);
     }
+
+    // Ask user if they want to play snap
+        // y - start game
+        // n - exit game
+    // start game set up
+        // shuffle deck
+    // game
+        // when user presses enter, deal first card - dealCard()
+        // a card is dealt each turn
+
+    // Deals out last card from deck, and then makes sure to remove it from deckOfCards
+    protected Card dealCard() {
+        Card lastCard = deckOfCards.getLast();
+        deckOfCards.remove(lastCard);
+        return lastCard;
+    }
+
 }
