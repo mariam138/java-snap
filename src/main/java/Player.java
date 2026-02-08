161dc1d5ce17c7317e4c23a6f754e5aa10d6ce2b
Mariam <mariam@Mariams-MacBook-Pro.local>
2026-02-08 20:31:41 +0000

    create Player class

---
+++ src/main/java/Player.java
@@ -0,0 +1,9 @@
+public class Player {
+    String playerName;
+    boolean playerWon;
+
+    public Player(String playerName, boolean playerWon) {
+        this.playerName = playerName;
+        this.playerWon = playerWon;
+    }
+}
