c962d6ea3f08ee659c7a8f31ae64dda8bd289c90
Mariam <mariam@Mariams-MacBook-Pro.local>
2026-02-08 20:57:46 +0000

    create getters and setters for both fields

--- src/main/java/Player.java
+++ src/main/java/Player.java
@@ -6,4 +6,20 @@ public class Player {
         this.playerName = playerName;
         this.playerWon = false;
     }
+
+    public String getPlayerName() {
+        return playerName;
+    }
+
+    public void setPlayerName(String playerName) {
+        this.playerName = playerName;
+    }
+
+    public boolean isPlayerWon() {
+        return playerWon;
+    }
+
+    public void setPlayerWon(boolean playerWon) {
+        this.playerWon = playerWon;
+    }
 }
