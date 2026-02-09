f233744e245aac5177c61f7c1273f8eb897f3672
Mariam <mariam@Mariams-MacBook-Pro.local>
2026-02-09 16:51:34 +0000

    print out card in pink text

--- src/main/java/Snap.java
+++ src/main/java/Snap.java
@@ -1,6 +1,13 @@
 public class Snap extends CardGame {
     Card prevCard = null;
 
+    // Custom colours for terminal text
+    // Declaring ANSI_RESET so that we can reset the color
+    public static final String ANSI_RESET = "\u001B[0m";
+
+    // Set colour to pink
+    public static final String ANSI_PINK = "\u001B[35m";
+
     protected Snap(String name) {
         super(name);
         this.noOfPlayers = 2;
@@ -29,7 +36,7 @@ public class Snap extends CardGame {
 
     protected boolean playTurn() {
         Card currCard = dealCard();
-        System.out.println(currCard);
+        System.out.println(ANSI_PINK + currCard + ANSI_RESET);
 
         boolean result = compareCardValues(prevCard, currCard);
         if (!result) {
