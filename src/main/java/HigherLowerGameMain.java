36b862dbcead34e640eb6d9c3b35cd0d8ee5c675
Mariam <mariam@Mariams-MacBook-Pro.local>
2026-02-10 16:18:44 +0000

    set up vars for game

--- src/main/java/HigherLowerGameMain.java
+++ src/main/java/HigherLowerGameMain.java
@@ -1,5 +1,11 @@
+import java.util.Scanner;
+
 public class HigherLowerGameMain {
     static void main(String[] args){
+        HigherLower higherLower = new HigherLower("higher or lower");
+        boolean game = true;
+
+        Scanner scanner = new Scanner(System.in);
 
     }
 }
