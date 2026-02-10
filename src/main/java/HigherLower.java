22239c7f11b2b93717abb62c112eb461f9145aee
Mariam <mariam@Mariams-MacBook-Pro.local>
2026-02-10 15:11:11 +0000

    create HigherLower game class

---
+++ src/main/java/HigherLower.java
@@ -0,0 +1,21 @@
+import java.util.ArrayList;
+
+public class HigherLower extends CardGame
+{
+    /*
+    - one player game
+    - shuffle the cards
+    - deal a card
+    - user must then guess if the next card is higher or lower
+    - if user is correct, the next card is dealt and this continues
+    - if user is incorrect, the game finishes and the user has lost
+     */
+    private Card prevCard = null;
+    private final ArrayList<Card> deckOfCards = getDeckOfCards();
+
+    protected HigherLower(String name) {
+        super(name);
+        setNoOfPlayers(1);
+    }
+
+}
