b242ba466d8498408197a7a6530cbd3bc0968351
Mariam <mariam@Mariams-MacBook-Pro.local>
2026-02-10 16:22:35 +0000

    set up game and create player instance

--- src/main/java/HigherLowerGameMain.java
+++ src/main/java/HigherLowerGameMain.java
@@ -1,11 +1,20 @@
 import java.util.Scanner;
 
 public class HigherLowerGameMain {
-    static void main(String[] args){
+    static void main(String[] args) throws InterruptedException {
         HigherLower higherLower = new HigherLower("higher or lower");
         boolean game = true;
 
         Scanner scanner = new Scanner(System.in);
+        System.out.printf("Hello! Let's play %s!\n", higherLower.getNameOfGame());
+        Thread.sleep(1000);
+        System.out.println("This is a one-player game.");
+        Thread.sleep(1000);
+        System.out.println("Player, please enter your name:");
+        String name = scanner.nextLine();
 
+        Player player = new Player(name);
+
+        System.out.printf("Okay %s, let's play higher or lower!\n", player.getPlayerName());
     }
 }
