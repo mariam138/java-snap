27ea823b16a594154dd6fedb1d262961cd937f86
Mariam <mariam@Mariams-MacBook-Pro.local>
2026-02-11 15:38:33 +0000

    refactor code to use GameUtils

--- src/main/java/SnapGameMain.java
+++ src/main/java/SnapGameMain.java
@@ -86,7 +86,7 @@ public class SnapGameMain {
                     Thread.sleep(1000);
                 }
             }
-            playAgain = askToPlayAgain(scanner);
+            playAgain = GameUtils.askToPlayAgain(scanner);
         }
     }
 
@@ -98,25 +98,6 @@ public class SnapGameMain {
                 return;
             }
             System.out.println("Invalid input. Please press 'enter' to deal a card.");
-        }
-    }
-
-//    While loop to ask user if they want to play again before game exits
-    private static boolean askToPlayAgain(Scanner scanner) {
-        System.out.println("Would you like to play again? (y/n)");
-
-        while (true) {
-            String playAgain = scanner.nextLine();
-            switch (playAgain) {
-                case "y":
-                    return true;
-                case "n":
-                    System.out.println("Goodbye!");
-                    return false;
-
-                default:
-                    System.out.println("Invalid input. Please enter y/n");
-            }
         }
     }
 }
