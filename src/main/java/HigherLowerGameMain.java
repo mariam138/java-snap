cc06f4e4534043027a5ec2072fd8bfb6a2a743ba
Mariam <mariam@Mariams-MacBook-Pro.local>
2026-02-11 15:33:07 +0000

    create askToPlayAgain helper method

--- src/main/java/HigherLowerGameMain.java
+++ src/main/java/HigherLowerGameMain.java
@@ -3,7 +3,6 @@ import java.util.Scanner;
 public class HigherLowerGameMain {
     static void main(String[] args) throws InterruptedException {
         HigherLower higherLower = new HigherLower("higher or lower");
-        boolean game = true;
 
         // Custom colours for terminal text
         // Declaring ANSI_RESET so that we can reset the colour
@@ -24,35 +23,42 @@ public class HigherLowerGameMain {
         System.out.printf("Okay %s, let's play higher or lower!\n", player.getPlayerName());
         Thread.sleep(2000);
 
-        higherLower.shuffleDeck();
-        System.out.println("The deck has been shuffled!");
-        Thread.sleep(1000);
-
-        while (game) {
-            System.out.printf("Current streak: %d\n", higherLower.getCorrectGuessCount());
-            System.out.println(" ");
+        boolean playAgain = true;
+        while (playAgain) {
+            boolean game = true;
+            higherLower.setCorrectGuessCount(0);
+            higherLower.shuffleDeck();
+            System.out.println("The deck has been shuffled!");
             Thread.sleep(1000);
-            Card currCard = higherLower.dealCard();
-            System.out.println(ANSI_YELLOW + currCard + ANSI_RESET);
 
-            System.out.println("Is the next card higher or lower?");
-            System.out.println("Enter either 'higher' or 'lower': ");
-
-//            Use String value from return and pass it into result
-            String guess = waitForValidInput(scanner);
-            boolean result = higherLower.playTurn(guess, player, currCard);
-
-            if (result) {
-                System.out.println("Correct!");
-            } else {
-                System.out.printf(
-                        "Sorry, wrong guess. The next card was %s.\n", higherLower.compareCardValues(currCard));
+            while (game) {
+                System.out.printf("Current streak: %d\n", higherLower.getCorrectGuessCount());
                 System.out.println(" ");
-                System.out.println(ANSI_YELLOW + higherLower.getNextCard() + ANSI_RESET);
-                System.out.println("Game over :(");
-                Thread.sleep(500);
-                game = false;
+                Thread.sleep(1000);
+                Card currCard = higherLower.dealCard();
+                System.out.println(ANSI_YELLOW + currCard + ANSI_RESET);
+
+                System.out.println("Is the next card higher or lower?");
+                System.out.println("Enter either 'higher' or 'lower': ");
+
+                //            Use String value from return and pass it into result
+                String guess = waitForValidInput(scanner);
+                boolean result = higherLower.playTurn(guess, player, currCard);
+
+                if (result) {
+                    System.out.println("Correct!");
+                } else {
+                    System.out.printf(
+                            "Sorry, wrong guess. The next card was %s.\n", higherLower.compareCardValues(currCard));
+                    System.out.println(" ");
+                    System.out.println(ANSI_YELLOW + higherLower.getNextCard() + ANSI_RESET);
+                    System.out.println("Game over :(");
+                    Thread.sleep(500);
+                    game = false;
+                }
             }
+
+            playAgain = askToPlayAgain(scanner);
         }
     }
 
@@ -63,6 +69,25 @@ public class HigherLowerGameMain {
                 return guess;
             }
             System.out.println("Invalid input. Please enter 'higher' or 'lower'.");
+        }
+    }
+
+    //    While loop to ask user if they want to play again before game exits
+    private static boolean askToPlayAgain(Scanner scanner) {
+        System.out.println("Would you like to play again? (y/n)");
+
+        while (true) {
+            String playAgain = scanner.nextLine();
+            switch (playAgain) {
+                case "y":
+                    return true;
+                case "n":
+                    System.out.println("Goodbye!");
+                    return false;
+
+                default:
+                    System.out.println("Invalid input. Please enter y/n");
+            }
         }
     }
 }
