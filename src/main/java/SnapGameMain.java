ef191e50f97c501c0527ce43c703a553913ced93
Mariam <mariam@Mariams-MacBook-Pro.local>
2026-02-11 15:19:15 +0000

    add askToPlayAgain helper method

--- src/main/java/SnapGameMain.java
+++ src/main/java/SnapGameMain.java
@@ -5,7 +5,6 @@ public class SnapGameMain {
     static void main(String[] args) throws InterruptedException {
 
         Snap snap = new Snap("snap");
-        boolean game = true;
 
         Scanner scanner = new Scanner(System.in);
         PrintWriter printWriter = new PrintWriter(System.out, true);
@@ -26,80 +25,68 @@ public class SnapGameMain {
         System.out.printf("Player 1: %s\n", playerOne.getPlayerName().toUpperCase());
         System.out.printf("Player 2: %s\n", playerTwo.getPlayerName().toUpperCase());
         System.out.println("Let's play!");
-        Thread.sleep(2000);
-        snap.shuffleDeck();
-        System.out.println("The deck has been shuffled!\n");
-
-        int turnNumber = 0;
-        Player currentPlayer;
         Thread.sleep(1000);
 
-        while (game) {
-            turnNumber++;
-            // Assign current player based on turn number
-            currentPlayer = (turnNumber % 2 == 0) ? playerTwo : playerOne;
+        boolean playAgain = true;
+        while (playAgain) {
+            Thread.sleep(1000);
+            boolean game = true;
+            snap.shuffleDeck();
+            System.out.println("The deck has been shuffled!\n");
 
-            System.out.printf("%s's TURN:\n", currentPlayer.getPlayerName().toUpperCase());
-            System.out.println("Press enter to deal a card.");
+            int turnNumber = 0;
+            Player currentPlayer;
+            Thread.sleep(1000);
 
-            // Use helper method to check that enter key has been pressed
-            waitForEnter(scanner);
-            boolean snapOccurred = snap.playTurn();
+            while (game) {
+                turnNumber++;
+                // Assign current player based on turn number
+                currentPlayer = (turnNumber % 2 == 0) ? playerTwo : playerOne;
 
-            if (snapOccurred) {
-                Thread.sleep(500);
-                System.out.printf(
-                        "%s, write 'SNAP' to win!\n",
-                        currentPlayer.getPlayerName().toUpperCase());
-                // Calculate elapsed time that user took to enter "snap"
-                // Then compare elapsed time to 2 seconds to determine whether user has won
-                long startTime = System.currentTimeMillis();
-                String snapInput = scanner.nextLine();
-                long elapsedTime = System.currentTimeMillis() - startTime;
-                if (elapsedTime <= 2000 && snapInput.equalsIgnoreCase("snap")) {
-                    printWriter.printf(
-                            "\uD83C\uDCCF %s WINS \uD83C\uDCCF \n",
-                            currentPlayer.getPlayerName().toUpperCase());
-                    currentPlayer.setPlayerWon(true);
+                System.out.printf("%s's TURN:\n", currentPlayer.getPlayerName().toUpperCase());
+                System.out.println("Press enter to deal a card.");
 
-                } else {
+                // Use helper method to check that enter key has been pressed
+                waitForEnter(scanner);
+                boolean snapOccurred = snap.playTurn();
+
+                if (snapOccurred) {
+                    Thread.sleep(500);
                     System.out.printf(
-                            "Sorry, %s lost :( \n",
+                            "%s, write 'SNAP' to win!\n",
                             currentPlayer.getPlayerName().toUpperCase());
-                    if (currentPlayer.equals(playerOne)) {
-                        playerTwo.setPlayerWon(true);
-                        System.out.printf(
-                                "\uD83C\uDCCF %s WINS \uD83C\uDCCF",
-                                playerTwo.getPlayerName().toUpperCase());
+                    // Calculate elapsed time that user took to enter "snap"
+                    // Then compare elapsed time to 2 seconds to determine whether user has won
+                    long startTime = System.currentTimeMillis();
+                    String snapInput = scanner.nextLine();
+                    long elapsedTime = System.currentTimeMillis() - startTime;
+                    if (elapsedTime <= 2000 && snapInput.equalsIgnoreCase("snap")) {
+                        printWriter.printf(
+                                "\uD83C\uDCCF %s WINS \uD83C\uDCCF \n",
+                                currentPlayer.getPlayerName().toUpperCase());
+                        currentPlayer.setPlayerWon(true);
+
                     } else {
-                        playerOne.setPlayerWon(true);
                         System.out.printf(
-                                "\uD83C\uDCCF %s WINS \uD83C\uDCCF",
-                                playerOne.getPlayerName().toUpperCase());
+                                "Sorry, %s lost :( \n",
+                                currentPlayer.getPlayerName().toUpperCase());
+                        if (currentPlayer.equals(playerOne)) {
+                            playerTwo.setPlayerWon(true);
+                            System.out.printf(
+                                    "\uD83C\uDCCF %s WINS \uD83C\uDCCF",
+                                    playerTwo.getPlayerName().toUpperCase());
+                        } else {
+                            playerOne.setPlayerWon(true);
+                            System.out.printf(
+                                    "\uD83C\uDCCF %s WINS \uD83C\uDCCF",
+                                    playerOne.getPlayerName().toUpperCase());
+                        }
                     }
+                    game = false;
+                    Thread.sleep(1000);
                 }
-                game = false;
             }
-        }
-
-        System.out.println("Would you like to play again? (y/n)");
-        String playAgain = scanner.nextLine();
-
-        switch (playAgain) {
-            case "y":
-                turnNumber = 0;
-                currentPlayer = null;
-                snap.shuffleDeck();
-                System.out.println("The deck has been shuffled!\n");
-                game = true;
-                Thread.sleep(1000);
-                break;
-            case "n":
-                System.out.println("Goodbye!");
-                scanner.close();
-                break;
-            default:
-                System.out.println("Invalid input. Please enter y/n");
+            playAgain = askToPlayAgain(scanner);
         }
     }
 
@@ -111,6 +98,25 @@ public class SnapGameMain {
                 return;
             }
             System.out.println("Invalid input. Please press 'enter' to deal a card.");
+        }
+    }
+
+//    While loop to ask user if they want to play again before game exits
+    private static boolean askToPlayAgain(Scanner scanner) {
+        System.out.println("Would you like to play again? (y/n)");
+
+        while (true) {
+            String playAgain = scanner.nextLine();
+            switch (playAgain) {
+                case "y":
+                    return true;
+                case "n":
+                    System.out.println("Goodbye!");
+                    return false;
+
+                default:
+                    System.out.println("Invalid input. Please enter y/n");
+            }
         }
     }
 }
